c7b6267f5a3098d7d85162447d2649f1bfd839d2
Zeinab <84691251+Zeinab-Amini@users.noreply.github.com>
2021-12-23 19:43:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random 
+
+random_int = random.randint(1,10)
+print(random_int)
+
+#0.0000000000 - 0.99999999999999
+#you can expained the floats 
+random_float = random.random() * 5 
+print(random_float)
+
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}%")
